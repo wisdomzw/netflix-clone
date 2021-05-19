f54e2d550aad767d90c548055f6b223a4b327519
Nyasha Nyaguwa <wisdomzw@gmail.com>
2021-05-19 19:06:25 +0200

    init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB43dpJoKQd6R6acVht46l_KEmc00qqpyo",
+    authDomain: "netflix-clone-7680f.firebaseapp.com",
+    projectId: "netflix-clone-7680f",
+    storageBucket: "netflix-clone-7680f.appspot.com",
+    messagingSenderId: "136347556075",
+    appId: "1:136347556075:web:195ae81ce8fe3288eec6ae"
+  };
